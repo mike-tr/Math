02ea44be598d69f477933ec2e22b0ad13d2f6825
Mike <Miket9717@gmail.com>
2020-12-28 23:16:40 +0200

    initial

---
+++ Assets/MatrixMath.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixMath {
+    public class Matrix : MonoBehaviour {
+
+        private double[, ] matrix;
+        private (int, int) _shape = (0, 0);
+        public (int, int) shape {
+            get => _shape;
+        }
+
+        public int rows {
+            get => _shape.Item1;
+        }
+
+        public int columns {
+            get => _shape.Item2;
+        }
+
+        public double this [int j, int i] {
+            get => matrix[j, i];
+            set => matrix[j, i] = value;
+        }
+
+        public static void SolveMatrix (double[][] matrix) {
+            SolveMatrix (new Matrix (matrix));
+        }
+
+        public static void SolveMatrix (float[][] matrix) {
+            SolveMatrix (new Matrix (matrix));
+        }
+        public static void SolveMatrix (int[][] matrix) {
+            SolveMatrix (new Matrix (matrix));
+        }
+
+        public static void SolveMatrix (Matrix matrix) {
+            // matrix
+            // [  [1,-2, 1,-4],
+            //    [0, 1, 2, 4],
+            //    [2, 3,-2, 2]]
+
+        }
+
+        public Matrix (int[][] matrix) {
+            if (matrix.Length == 0) {
+                this.matrix = new double[0, 0];
+                return;
+            }
+            this.matrix = new double[matrix.Length, matrix[0].Length];
+            for (int i = 0; i < matrix.Length; i++) {
+                for (int j = 0; j < matrix[0].Length; j++) {
+                    this.matrix[i, j] = matrix[i][j];
+                }
+            }
+        }
+        public Matrix (float[][] matrix) {
+            if (matrix.Length == 0) {
+                this.matrix = new double[0, 0];
+                return;
+            }
+            this.matrix = new double[matrix.Length, matrix[0].Length];
+            for (int i = 0; i < matrix.Length; i++) {
+                for (int j = 0; j < matrix[0].Length; j++) {
+                    this.matrix[i, j] = matrix[i][j];
+                }
+            }
+        }
+        public Matrix (double[][] matrix) {
+            if (matrix.Length == 0) {
+                this.matrix = new double[0, 0];
+                return;
+            }
+            this._shape = (matrix.Length, matrix[0].Length);
+            this.matrix = new double[shape.Item1, shape.Item2];
+
+            for (int i = 0; i < shape.Item1; i++) {
+                for (int j = 0; j < shape.Item1; j++) {
+                    this.matrix[i, j] = matrix[i][j];
+                }
+            }
+        }
+    }
+}
---
+++ Assets/myPlaneTester.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myPlaneTester : MonoBehaviour {
+    public Transform v1, v2, v3;
+    public Transform target;
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+
+    static float vecMultiplication (Vector3 v1, Vector3 v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
+    Vector3 projectAOnB (Vector3 A, Vector3 B) {
+        float size = vecMultiplication (A, B) / vecMultiplication (B, B);
+        return size * B;
+    }
+
+    static Vector3 complete (Vector3 b1, Vector3 b2) {
+
+        return Vector3.zero;
+    }
+
+    private void OnDrawGizmos () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine (v1.position, v2.position);
+        Gizmos.color = Color.blue;
+
+        var dir = v2.position - v1.position;
+        var origin = v1.position + projectAOnB (v3.position - v1.position, dir);
+        Gizmos.DrawSphere (origin, 0.2f);
+        Gizmos.DrawLine (origin, v3.position);
+        //Gizmos.DrawSphere (projectAOnB (v1.position + v3.position, v1.position - v2.position), 1);
+        //Gizmos.DrawCube (v1.position, Vector3.one * 10);
+    }
+}
